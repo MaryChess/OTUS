ed96d3f81d6f765f2dc1fc70315311c7fa91dd4d
MaryChess <72669536+MaryChess@users.noreply.github.com>
2020-10-10 22:42:19 +0300

    Update HW3.py

--- HW3.py
+++ HW3.py
@@ -4,6 +4,15 @@
 #Подробную инструкцию по выполнению заданий смотрите в документе
 #https://docs.google.com/document/d/1Ezkk_vtjP3imz_-Ij0aglcyiWSbujgJC0pLbdcP4c0I/edit?usp=sharing
 
+# С помощью Spark соберите агрегат по районам (поле district) со следующими метриками:
+# 1.	crimes_total - общее количество преступлений в этом районе
+# 2.	crimes_monthly - медиана числа преступлений в месяц в этом районе
+# 3.	frequent_crime_types - три самых частых crime_type за всю историю наблюдений в этом районе, объединенных через запятую с одним пробелом “, ” , расположенных в порядке убывания частоты
+# a.	crime_type - первая часть NAME из таблицы offense_codes, разбитого по разделителю “-” (например, если NAME “BURGLARY - COMMERICAL - ATTEMPT”, то crime_type “BURGLARY”)
+# 4.	lat - широта координаты района, расчитанная как среднее по всем широтам инцидентов
+# 5.	lng - долгота координаты района, расчитанная как среднее по всем долготам инцидентов
+# Для джойна со справочником необходимо использовать broadcast.
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.window import Window
